30a7844692b5224eacf1d12f7d2ad7f60221e9f1
廖政 <813144314@qq.com>
2021-11-11 23:32:16 +0800

    feat: 实现effect、reactive、依赖收集、依赖触发

---
+++ src/reactivity/effect.ts
@@ -0,0 +1,42 @@
+class ReactiveEffect {
+  private _fn: any
+  constructor(fn) {
+    this._fn = fn
+  }
+  run() {
+    activeEffect = this
+    return this._fn()
+  }
+}
+
+const targetMap = new Map()
+export function track(target, key) {
+  let depsMap = targetMap.get(target)
+  if (!depsMap) {
+    depsMap = new Map()
+    targetMap.set(target, depsMap)
+  }
+
+  let dep = depsMap.get(key)
+  if (!dep) {
+    dep = new Set()
+    depsMap.set(key, dep)
+  }
+
+  dep.add(activeEffect)
+}
+
+export function trigger(target, key) {
+  const depsMap = targetMap.get(target)
+  const dep = depsMap.get(key)
+  dep.forEach(effect => effect.run())
+}
+
+let activeEffect;
+export function effect(fn) {
+  const _effect = new ReactiveEffect(fn)
+
+  _effect.run()
+
+  return _effect.run.bind(_effect)
+}
---
+++ src/reactivity/tests/effect.spec.ts
@@ -0,0 +1,32 @@
+import { reactive } from '../reactive';
+import { effect } from '../effect';
+describe('effect', () => {
+  it("happy path", () => {
+    const user = reactive({
+      age: 10,
+    })
+
+    let nextAge;
+    effect(() => {
+      nextAge = user.age + 1
+    })
+
+    expect(nextAge).toBe(11)
+
+    user.age++
+    expect(nextAge).toBe(12)
+  })
+
+  it("", () => {
+    let foo = 10
+    const runner = effect(() => {
+      foo++
+      return 'foo'
+    })
+
+    expect(foo).toBe(11)
+    const r = runner()
+    expect(foo).toBe(12)
+    expect(r).toBe('foo')
+  })
+})
