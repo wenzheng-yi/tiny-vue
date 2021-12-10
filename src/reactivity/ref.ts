9e90ca4100113c6a961fecc905a7350f2304bc52
廖政 <813144314@qq.com>
2021-12-10 09:12:56 +0800

    refactor: 重构ref

--- src/reactivity/ref.ts
+++ src/reactivity/ref.ts
@@ -8,26 +8,31 @@ class RefImpl {
   private _rawValue: any
   constructor(raw) {
     this._rawValue = raw
-    this._value = isObject(raw) ? reactive(raw) : raw
+    this._value = convert(raw)
     this.dep = new Set()
   }
   get value() {
     // 收集依赖
-    if (isTracking()) {
-      trackEffects(this.dep)
-    }
+    trackRefEffects(this)
     return this._value
   }
   set value(newValue) {
     // 一定是先修改了值的，触发依赖
     if (hasChanged(newValue, this._rawValue)) {
       this._rawValue = newValue
-      this._value = isObject(newValue) ? reactive(newValue) : newValue
+      this._value = convert(newValue)
       triggerEffects(this.dep)
     }
   }
 }
-
+function  trackRefEffects(ref) {
+  if (isTracking()) {
+    trackEffects(ref.dep)
+  }
+}
+function convert(value) {
+  return isObject(value) ? reactive(value) : value
+}
 export function ref(raw) {
   return new RefImpl(raw)
 }
