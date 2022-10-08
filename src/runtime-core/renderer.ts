da67dca0e34845c0d1c1cf03db554cade6f2799d
liaozheng-yi <813144314@qq.com>
2022-10-09 00:08:12 +0800

    feat: diff算法第三部分-中间部分-移动节点和创建新节点

--- src/runtime-core/renderer.ts
+++ src/runtime-core/renderer.ts
@@ -162,15 +162,22 @@ export function createRenderer(options) {
       // 中间对比
       let s1 = i,
         s2 = i
+
+      const toBePatched = e2 - s2 + 1
+      let patched = 0
+
       const keyToNewIndexMap = new Map() // 记录新children中间部分的节点index
+      const newIndexToOldIndexMap = new Array(toBePatched).fill(0) // 创建一个定长的数组
+
+      let moved = false
+      let maxNewIndexSoFar = 0
+
       for (let i = s2; i <= e2; i++) {
         // 此处的 nextChild/prevChild 是相对于更新前、更新后
         const nextChild = c2[i]
         keyToNewIndexMap.set(nextChild.key, i)
       }
 
-      const toBePatched = e2 - s2 + 1
-      let patched = 0
       for (let i = s1; i <= e1; i++) {
         const prevChild = c1[i]
 
@@ -199,9 +206,39 @@ export function createRenderer(options) {
         if (newIndex === undefined) {
           hostRemove(prevChild.el)
         } else {
-          // 如果存在，则对它继续深度遍历
+          if (newIndex >= maxNewIndexSoFar) {
+            maxNewIndexSoFar = newIndex
+          } else {
+            moved = true
+          }
+
+          // 如果存在，则记录新老位置
+          // +1是因为i可能为0，但0和数据初始化的值相同，会被认为老节点不存在了
+          newIndexToOldIndexMap[newIndex - s2] = i + 1
+          // 对它继续深度遍历
           patch(prevChild, c2[newIndex], container, parentComponent, null)
           patched++
+        }
+      }
+
+      const increasingNewIndexSequence = moved
+        ? getSequence(newIndexToOldIndexMap)
+        : []
+      let j = increasingNewIndexSequence.length - 1
+
+      for (let i = toBePatched - 1; i >= 0; i--) {
+        const nextIndex = i + s2
+        const nextChild = c2[nextIndex]
+        const anchor = nextIndex + 1 < l2 ? c2[nextIndex + 1].el : null
+        if (newIndexToOldIndexMap[i] === 0) {
+          patch(null, nextChild, container, parentComponent, anchor)
+        } else if (moved) {
+          if (j < 0 || i !== increasingNewIndexSequence[j]) {
+            console.log('移动位置')
+            hostInsert(nextChild.el, container, anchor)
+          } else {
+            j--
+          }
         }
       }
     }
@@ -303,4 +340,45 @@ export function createRenderer(options) {
   return {
     createApp: createAppAPI(render),
   }
+}
+
+function getSequence(arr) {
+  const p = arr.slice()
+  const result = [0]
+  let i, j, u, v, c
+  const len = arr.length
+  for (i = 0; i < len; i++) {
+    const arrI = arr[i]
+    if (arrI !== 0) {
+      j = result[result.length - 1]
+      if (arr[j] < arrI) {
+        p[i] = j
+        result.push(i)
+        continue
+      }
+      u = 0
+      v = result.length - 1
+      while (u < v) {
+        c = (u + v) >> 1
+        if (arr[result[c]] < arrI) {
+          u = c + 1
+        } else {
+          v = c
+        }
+      }
+      if (arrI < arr[result[u]]) {
+        if (u > 0) {
+          p[i] = result[u - 1]
+        }
+        result[u] = i
+      }
+    }
+  }
+  u = result.length
+  v = result[u - 1]
+  while (u-- > 0) {
+    result[u] = v
+    v = p[v]
+  }
+  return result
 }
