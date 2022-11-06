777986937a8327f30f57af16a2a7359e5db29169
liaozheng-yi <813144314@qq.com>
2022-11-06 18:23:04 +0800

    feat: 解析element

--- src/compiler-core/src/parse.ts
+++ src/compiler-core/src/parse.ts
@@ -1,5 +1,8 @@
 import { NodeTypes } from './ast'
-
+const enum TagType {
+  START,
+  END,
+}
 export function baseParse(content: string) {
   const context = createParserContext(content)
   return createRoot(parseChildren(context))
@@ -17,8 +20,13 @@ function createRoot(children) {
 function parseChildren(context): any {
   const nodes: any = []
   let node
-  if (context.source.startsWith('{{')) {
+  const s = context.source
+  if (s.startsWith('{{')) {
     node = parseInterpolation(context)
+  } else if (s[0] === '<') {
+    if (/[a-z]/i.test(s[1])) {
+      node = parseElement(context)
+    }
   }
   nodes.push(node)
   return nodes
@@ -46,4 +54,24 @@ function parseInterpolation(context) {
 
 function advanceBy(context: any, length) {
   context.source = context.source.slice(length)
+}
+function parseElement(context: any) {
+  const element = parseTag(context, TagType.START)
+  parseTag(context, TagType.END)
+
+  return element
+}
+function parseTag(context: any, type: TagType) {
+  // 1. 解析tag
+  const match: any = /^<\/?([a-z]*)/i.exec(context.source)
+  const tag = match[1]
+  // 2. 删除处理完成的代码
+  advanceBy(context, match[0].length)
+  advanceBy(context, 1)
+  console.log(context.source)
+  if (type === TagType.END) return
+  return {
+    type: NodeTypes.ELEMENT,
+    tag,
+  }
 }
