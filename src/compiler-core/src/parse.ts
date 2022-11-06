bffed451a7fecbc0849e6bd96bfd5de65849b914
liaozheng-yi <813144314@qq.com>
2022-11-06 11:38:10 +0800

    feat: 解析插值功能

---
+++ src/compiler-core/src/parse.ts
@@ -0,0 +1,49 @@
+import { NodeTypes } from './ast'
+
+export function baseParse(content: string) {
+  const context = createParserContext(content)
+  return createRoot(parseChildren(context))
+}
+function createParserContext(content: string) {
+  return {
+    source: content,
+  }
+}
+function createRoot(children) {
+  return {
+    children,
+  }
+}
+function parseChildren(context): any {
+  const nodes: any = []
+  let node
+  if (context.source.startsWith('{{')) {
+    node = parseInterpolation(context)
+  }
+  nodes.push(node)
+  return nodes
+}
+function parseInterpolation(context) {
+  const openDelimiter = '{{'
+  const closeDelimiter = '}}'
+
+  const closeIndex = context.source.indexOf(closeDelimiter)
+  advanceBy(context, openDelimiter.length)
+
+  const rawContentLength = closeIndex - openDelimiter.length
+  const rawContent = context.source.slice(0, rawContentLength)
+  const content = rawContent.trim()
+  advanceBy(context, rawContentLength + closeDelimiter.length)
+
+  return {
+    type: NodeTypes.INTERPOLATION,
+    content: {
+      type: NodeTypes.SIMPLE_EXPRESSION,
+      content: content,
+    },
+  }
+}
+
+function advanceBy(context: any, length) {
+  context.source = context.source.slice(length)
+}
