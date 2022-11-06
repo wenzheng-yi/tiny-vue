c444849d892b426ff406573899529db71cdab22f
liaozheng-yi <813144314@qq.com>
2022-11-06 23:45:16 +0800

    feat: 解析text

--- src/compiler-core/src/parse.ts
+++ src/compiler-core/src/parse.ts
@@ -28,6 +28,9 @@ function parseChildren(context): any {
       node = parseElement(context)
     }
   }
+  if (!node) {
+    node = parseText(context)
+  }
   nodes.push(node)
   return nodes
 }
@@ -39,9 +42,9 @@ function parseInterpolation(context) {
   advanceBy(context, openDelimiter.length)
 
   const rawContentLength = closeIndex - openDelimiter.length
-  const rawContent = context.source.slice(0, rawContentLength)
+  const rawContent = parseTextData(context, rawContentLength)
   const content = rawContent.trim()
-  advanceBy(context, rawContentLength + closeDelimiter.length)
+  advanceBy(context, closeDelimiter.length)
 
   return {
     type: NodeTypes.INTERPOLATION,
@@ -74,4 +77,16 @@ function parseTag(context: any, type: TagType) {
     type: NodeTypes.ELEMENT,
     tag,
   }
+}
+function parseText(context: any): any {
+  const content = parseTextData(context, context.source.length)
+  return {
+    type: NodeTypes.TEXT,
+    content,
+  }
+}
+function parseTextData(context: any, length) {
+  const content = context.source.slice(0, length)
+  advanceBy(context, content.length)
+  return content
 }
